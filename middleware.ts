6b63fc1f3cdf11e948bc5d1dd008cbee7f99954e
Tanmay Rana <tanmayrana2001@gmail.com>
2024-11-06 10:53:40 +0530

    All code add 3rd time commit

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+const isProtectedRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api/webhooks(.*)",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isProtectedRoute(req)) await auth.protect();
